0751f970c747cfa1e409b6d3bbfa46020a6fecb2
lesimpleliott <lesimpleliott@gmail.com>
2025-04-07 17:58:18 +0200

    Add custom error handling with AppError for addPost Form

--- src/lib/serverActions/blog/postServerActions.js
+++ src/lib/serverActions/blog/postServerActions.js
@@ -2,7 +2,9 @@
 
 import { Post } from "@/lib/models/post";
 import { Tag } from "@/lib/models/tag";
+import { sessionInfos } from "@/lib/serverMethods/session/sessionMethods";
 import connectToDB from "@/lib/utils/db/connectToDB";
+import AppError from "@/lib/utils/errorHandling/customError";
 import createDOMPurify from "dompurify";
 import { JSDOM } from "jsdom";
 import { marked } from "marked";
@@ -22,10 +24,38 @@ export async function addPost(formData) {
   const { title, markdownArticle, tags } = Object.fromEntries(formData);
 
   try {
-    await connectToDB();
+    // Validation du titre côté serveur
+    if (typeof title !== "string" || title.trim().length < 3) {
+      throw new AppError("Invalid data");
+    }
+    // Validation de l'article côté serveur
+    if (
+      typeof markdownArticle !== "string" ||
+      markdownArticle.trim().length === 0
+    ) {
+      throw new AppError("Invalid data");
+    }
+
+    await connectToDB(); // Connexion à la base de données
+
+    // Vérification de la session côté serveur
+    const session = sessionInfos();
+    if (!session.success) {
+      throw new AppError("Authentification required");
+    }
 
     // Gestion des tags
+
+    // Vérification de l'existence des tags
+    if (typeof tags !== "string" || tags.trim().length === 0) {
+      throw new AppError("Invalid data");
+    }
     const tagNamesArray = JSON.parse(tags);
+    // Vérification du format des tags
+    if (!Array.isArray(tagNamesArray)) {
+      throw new AppError("Invalid tags format, must be an array");
+    }
+
     const tagIds = await Promise.all(
       tagNamesArray.map(async (tagName) => {
         const normalizedTagName = tagName.trim().toLowerCase();
@@ -77,7 +107,14 @@ export async function addPost(formData) {
 
     return { success: true, slug: savedPost.slug };
   } catch (err) {
-    console.error("Error while saving post:", err);
-    throw new Error(err.message || "An error occurred while saving the post");
-  }
+    console.error("Error while saving post:", err); // côté TErminal/serveur
+    
+    // Gestion des erreurs et renvoi d'un message d'erreur 'personnalisé'
+    if(err instanceof AppError ) {
+      throw err
+    }
+
+    throw new Error("Error while saving post"); // côté client
+
+    }
 }
